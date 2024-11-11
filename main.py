b38f34b45a7c1ce7f311a104b68c1eb30e052259
mruggeri-flx <martin.ruggeri@flexibility.com.ar>
2024-11-11 19:13:15 -0300

    add pdf

--- main.py
+++ main.py
@@ -1,34 +1,48 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File, Form
-from fastapi.responses import FileResponse, StreamingResponse
+from fastapi.responses import FileResponse
 from uuid import UUID
 from reportlab.lib.pagesizes import letter
-import os
+
 
 from main_service import main_service
 
 app = FastAPI()
 
-@app.post("/generate_pdf")
-async def upload_csv(id: UUID = Form(...), csv_file: UploadFile = File(...)):
+from fastapi import FastAPI, HTTPException, UploadFile, File, Form, Body
+from fastapi.responses import FileResponse
+from uuid import UUID
+from main_service import main_service
+
+app = FastAPI()
+
+# Primera API: recibe un ID y un archivo CSV, y devuelve el path del PDF generado
+@app.post("/generate_pdf_path")
+async def generate_pdf_path(id: UUID = Form(...), csv_file: UploadFile = File(...)):
     # Validar el tipo de archivo
     if csv_file.content_type != "text/csv":
         raise HTTPException(status_code=400, detail="El archivo debe ser un CSV")
     
-    # Leer el contenido del archivo
+    # Leer el contenido del archivo y generar el PDF
     try:
         pdf_filename = f"{id}_pdf.pdf"
         pdf_path = main_service(csv_file.file, pdf_filename)
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error al leer el CSV: {str(e)}")
+        raise HTTPException(status_code=400, detail=f"Error al procesar el CSV: {str(e)}")
+    return {"pdf_path": pdf_path}
 
-    # Enviar el PDF como respuesta usando StreamingResponse
-    return FileResponse(
-        path=pdf_path,
-        media_type="application/pdf",
-        filename=pdf_filename
-    )
+# Segunda API: recibe el path del PDF en el cuerpo de la solicitud y devuelve el PDF
+@app.post("/get_pdf")
+async def get_pdf(pdf_path: str = Body(...)):
+    # Verificar si el archivo existe
+    try:
+        return FileResponse(
+            path=pdf_path,
+            media_type="application/pdf",
+            filename=pdf_path.split("/")[-1]
+        )
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=f"Error al encontrar el archivo: {str(e)}")
 
 if __name__ == "__main__":
     import uvicorn
-    # para poder hacer debug en el código
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
